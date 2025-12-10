1a604666d1d27174c329cd62e344a9a253ebedf0
callitask <callitask@gmail.com>
2025-12-10 14:51:46 +0530

    Error: Build failed due to unused 'useMemo' import in NewsIntelligenceWidget.js. Fix: Removed unused 'useMemo' from imports to satisfy CI strict linting rules and restore build stability.

--- src/components/news/NewsIntelligenceWidget.js
+++ src/components/news/NewsIntelligenceWidget.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import NewsCard from './NewsCard';
 import './NewsCard.css';
